c4dfb2c7863d46b3939fd5126c4edb5bea961673
Your Name <you@example.com>
2025-06-22 20:12:08 +0530

    updated : navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link, useResolvedPath } from "react-router-dom";
 import { ShoppingCartIcon } from "lucide-react";
 import Themeselector from "./ThemeSelector";
+import { useProductStore } from "../store/useProductStore";
 
 function Navbar() {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === "/";
+  const [products] = useProductStore();
 
   return (
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
@@ -32,7 +34,7 @@ function Navbar() {
                 <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
                   <ShoppingCartIcon className="size-5" />
                   <span className="badge badge-sm badge-primary indicator-item">
-                    8
+                    {products.length}
                   </span>
                 </div>
               </div>
